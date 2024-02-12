519c159a49691575e6435298eec3405696fb490d
jca <118293506+JCA-STEPH@users.noreply.github.com>
2024-02-12 17:10:31 +0300

    error label

--- pages/admin/login.php
+++ pages/admin/login.php
@@ -31,6 +31,9 @@
             </div>
           </div>
             <form action="log.php" method="post" class="d-block" name="form">
+              <div >
+                <p class="text-danger"> Authentification incorect</p>
+              </div>
               <div class="form-floating mb-3">
                 <input type="text" name="login" id="inputMail" value="alexAdmin" class="form-control" placeholder="">
                 <label for="inputMail">Login</label>
