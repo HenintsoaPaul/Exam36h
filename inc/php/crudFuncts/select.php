eb19a5bdd928e3aaff5fa625362003d146465ea7
tsoa <henintsoapaul@gmail.com>
2024-02-12 13:09:41 +0300

    Init fonction login.

---
+++ inc/php/crudFuncts/select.php
@@ -0,0 +1,34 @@
+<?php
+
+/**
+ * Return the number of row matching Pseudo+Password. If 1, SUCCESS. Else, FAILED.
+ * @param string $pseudo
+ * @param string $password
+ * @return int|string
+ */
+function checkLogin( $connection, $pseudo, $password )
+{
+    $format = "SELECT * FROM Users WHERE pseudo = '%s' AND password = '%s'";
+    $query = sprintf( $format, $pseudo, $password );
+    $result = false;
+
+    try {
+        $result = mysqli_query( $connection, $query );
+
+        if ( !$result ) {
+            // Display the MySQL error message
+            $errorMessage = mysqli_error( $connection );
+            echo "MySQL error: $errorMessage";
+            throw new Exception( 'Query failed: ' . $errorMessage );
+        }
+    }
+    catch ( Exception $e ) {
+        // Handle the exception, e.g., log the error message
+        error_log( $e->getMessage() );
+        // Optionally, you can display the error message to the user in a user-friendly way
+        echo "An error occurred: " . htmlspecialchars( $e->getMessage() );
+    }
+
+    // Return the number of rows returned by the query
+    return mysqli_num_rows( $result );
+}
---
+++ sql/schema.sql
@@ -0,0 +1,3 @@
+# CREATE DATABASE the;
+# USE the;
+
