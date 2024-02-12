55ee5baab4af8094af6b4f9c1d2d6f2efb3ab19e
tsoa <henintsoapaul@gmail.com>
2024-02-12 21:37:28 +0300

    Add input verifications.

--- pages/admin/traitements/addCueilleur.php
+++ pages/admin/traitements/addCueilleur.php
@@ -2,16 +2,20 @@
 require_once '../../../inc/php/connection.php';
 require_once '../../../inc/php/crudFuncts/create.php';
 
+// Verify inputs
+if ( !isset( $_POST['naissance'] ) ) $error = "naissance cannot be null!";
+elseif ( !isset( $_POST['nomInput'] ) ) $error = "nomInput cannot be null!";
+elseif ( !isset( $_POST['genreInput'] ) ) $error = "genreInput cannot be null!";
 
-$link = "../insertion-ceuilleur.php?message";
-
+// Redirect if a value is null
+$link = "../insertion-cueilleur.php?message";
+if ( isset( $error ) ) {
+    header( "Location:$link=$error" );
+}
 
 $dtn = $_POST['naissance'];
 $nom = $_POST['nomInput'];
 $genre = $_POST['genreInput'];
-
-
-
 
 // exe query
 $connection = db_connect();
