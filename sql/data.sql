36e240fe4e39b2e502c88ce0860ed94854955e20
sergianafr <sergiravelo@gmail.com>
2024-02-12 14:44:56 +0300

    Data test

--- sql/data.sql
+++ sql/data.sql
@@ -1,1 +1,18 @@
 INSERT INTO the_status (NomStatu) VALUES ('Admin'), ('User');
+INSERT INTO the_Genres VALUES (null, 'Homme'), (null, 'Femme');
+
+INSERT INTO the_VarietesThes (null, 'Vert', 2.5, 0.3),
+                            (null, 'Noir', 2.5, 0.3) ,
+                            (null, 'Citronelle', 5.5, 0.1);
+
+INSERT INTO the_CategoriesDepenses VALUES (null, 'Engrais'),
+                                            (null, 'Carburant'),
+                                            (null, 'Logistique');
+
+INSERT INTO the_Parcelle VALUES (null, 30, 1), (null, 20, 2), (null, 20, 3);
+
+INSERT INTO the_Cueilleurs VALUES (null, 'Alex', '2000-02-09', 1), (null, 'Henintsoa', '2000-02-03', 1);
+
+INSERT INTO the_Users VALUES (null, 'alexAdmin', 'admin', 1);
+INSERT INTO the_Users VALUES (null, 'henints', 'user', 2);
+
