1183073bb5c95a2a5cc76c5658eea808d58ac244
tsoa <henintsoapaul@gmail.com>
2024-02-13 01:14:01 +0300

    Resolve insert cueillette problem.

--- inc/js/functions.js
+++ inc/js/functions.js
@@ -1,164 +1,174 @@
 function getxhr() {
-  var xhr;
-    try { xhr = new ActiveXObject('Msxml2.XMLHTTP'); }
-    catch (e) {
-      try { xhr = new ActiveXObject('Microsoft.XMLHTTP'); }
-      catch (e2) {
-        try { xhr = new XMLHttpRequest(); }
-        catch (e3) { xhr = false; }
-      }
+    var xhr;
+    try {
+        xhr = new ActiveXObject('Msxml2.XMLHTTP');
+    } catch (e) {
+        try {
+            xhr = new ActiveXObject('Microsoft.XMLHTTP');
+        } catch (e2) {
+            try {
+                xhr = new XMLHttpRequest();
+            } catch (e3) {
+                xhr = false;
+            }
+        }
     }
-  return xhr;
+    return xhr;
 }
+
 function getTab(link) {
-  var xhr; 
-  try {  xhr = new ActiveXObject('Msxml2.XMLHTTP');   }
-  catch (e) 
-  {
-      try {   xhr = new ActiveXObject('Microsoft.XMLHTTP'); }
-      catch (e2) 
-      { 
-      try {  xhr = new XMLHttpRequest();  }
-      catch (e3) {  xhr = false;   }
-      }
-  }
-  var retour=null;
-  xhr.onreadystatechange  = function(){ 
-      if(xhr.status  == 200) {
-          retour = JSON.parse(xhr.responseText);
-          // console.log(xhr.responseText);
-      }
-  }
-  xhr.open("GET", link,  false); 
-  xhr.send(null);  
-  return retour;
-} 
-function getRestePoids(herf,date, idParcelle) {
-  var xhr = getxhr();
-  var Data = new FormData();
-  Data.append("idParcelle", idParcelle);
-  Data.append("date", date);
-
-  var retour;
-
-  xhr.onreadystatechange = function () {
-      if (xhr.readyState == 4) {
-        if(xhr.status == 200) {
-          // console.log(xhr.responseText);
-          console.log(xhr.responseText);
-          retour = JSON.parse(xhr.responseText);
-        } else {
-          console.log("error : " + xhr.status);
+    var xhr;
+    try {
+        xhr = new ActiveXObject('Msxml2.XMLHTTP');
+    } catch (e) {
+        try {
+            xhr = new ActiveXObject('Microsoft.XMLHTTP');
+        } catch (e2) {
+            try {
+                xhr = new XMLHttpRequest();
+            } catch (e3) {
+                xhr = false;
+            }
         }
-      }
-    };
-
-  xhr.open("POST", herf, false);
-  xhr.send(Data);
-
-  return retour;
-}
-function getPoidsCueilli(herf,dateDebut, dateFin) {
-  var xhr = getxhr();
-  var Data = new FormData();
-  Data.append("dateDebut", dateDebut);
-  Data.append("dateFin", dateFin);
-
-  var retour;
-
-  xhr.onreadystatechange = function () {
-      if (xhr.readyState == 4) {
-        if(xhr.status == 200) {
-          // console.log(xhr.responseText);
-          console.log(xhr.responseText);
-          retour = JSON.parse(xhr.responseText);
-        } else {
-          console.log("error : " + xhr.status);
+    }
+    var retour = null;
+    xhr.onreadystatechange = function () {
+        if (xhr.status === 200) {
+            retour = JSON.parse(xhr.responseText);
+            // console.log(xhr.responseText);
         }
-      }
-    };
-
-  xhr.open("POST", herf, false);
-  xhr.send(Data);
-
-  return retour;
+    }
+    xhr.open("GET", link, false);
+    xhr.send(null);
+    return retour;
 }
 
-function getPrixRevient(herf,dateDebut, dateFin) {
-  var xhr = getxhr();
-  var Data = new FormData();
-  Data.append("dateDebut", dateDebut);
-  Data.append("dateFin", dateFin);
+function getRestePoids(herf, date, idParcelle) {
+    let xhr = getxhr();
 
-  var retour;
+    const Data = new FormData();
+    Data.append("idParcelle", idParcelle);
+    Data.append("date", date);
 
-  xhr.onreadystatechange = function () {
-      if (xhr.readyState == 4) {
-        if(xhr.status == 200) {
-          // console.log(xhr.responseText);
-          console.log(xhr.responseText);
-          retour = JSON.parse(xhr.responseText);
-        } else {
-          console.log("error : " + xhr.status);
+    let retour;
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState === 4) {
+            if (xhr.status === 200) {
+                console.log(xhr.responseText);
+                retour = JSON.parse(xhr.responseText);
+            } else {
+                console.log("error : " + xhr.status);
+            }
         }
-      }
     };
 
-  xhr.open("POST", herf, false);
-  xhr.send(Data);
+    xhr.open("POST", herf, false);
+    xhr.send(Data);
 
-  return retour;
+    return retour;
 }
 
-function getPoidsRestants(herf,dateDebut, dateFin, idParcelle) {
-  var xhr = getxhr();
-  var Data = new FormData();
-  Data.append("dateDebut", dateDebut);
-  Data.append("dateFin", dateFin);
-  Data.append("idParcelle", idParcelle);
+function getPoidsCueilli(herf, dateDebut, dateFin) {
+    var xhr = getxhr();
+    var Data = new FormData();
+    Data.append("dateDebut", dateDebut);
+    Data.append("dateFin", dateFin);
 
+    var retour;
 
-  var retour;
-
-  xhr.onreadystatechange = function () {
-      if (xhr.readyState == 4) {
-        if(xhr.status == 200) {
-          // console.log(xhr.responseText);
-          console.log(xhr.responseText);
-          retour = JSON.parse(xhr.responseText);
-        } else {
-          console.log("error : " + xhr.status);
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState == 4) {
+            if (xhr.status == 200) {
+                // console.log(xhr.responseText);
+                console.log(xhr.responseText);
+                retour = JSON.parse(xhr.responseText);
+            } else {
+                console.log("error : " + xhr.status);
+            }
         }
-      }
     };
 
-  xhr.open("POST", herf, false);
-  xhr.send(Data);
+    xhr.open("POST", herf, false);
+    xhr.send(Data);
 
-  return retour;
+    return retour;
+}
+
+function getPrixRevient(herf, dateDebut, dateFin) {
+    var xhr = getxhr();
+    var Data = new FormData();
+    Data.append("dateDebut", dateDebut);
+    Data.append("dateFin", dateFin);
+
+    var retour;
+
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState == 4) {
+            if (xhr.status == 200) {
+                // console.log(xhr.responseText);
+                console.log(xhr.responseText);
+                retour = JSON.parse(xhr.responseText);
+            } else {
+                console.log("error : " + xhr.status);
+            }
+        }
+    };
+
+    xhr.open("POST", herf, false);
+    xhr.send(Data);
+
+    return retour;
+}
+
+function getPoidsRestants(herf, dateDebut, dateFin, idParcelle) {
+    var xhr = getxhr();
+    var Data = new FormData();
+    Data.append("dateDebut", dateDebut);
+    Data.append("dateFin", dateFin);
+    Data.append("idParcelle", idParcelle);
+
+
+    var retour;
+
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState == 4) {
+            if (xhr.status == 200) {
+                // console.log(xhr.responseText);
+                console.log(xhr.responseText);
+                retour = JSON.parse(xhr.responseText);
+            } else {
+                console.log("error : " + xhr.status);
+            }
+        }
+    };
+
+    xhr.open("POST", herf, false);
+    xhr.send(Data);
+
+    return retour;
 }
 
 function getAllParcelle(herf) {
-  var xhr = getxhr();
-  var Data = new FormData();
+    var xhr = getxhr();
+    var Data = new FormData();
 
-  var retour;
+    var retour;
 
-  xhr.onreadystatechange = function () {
-      if (xhr.readyState == 4) {
-        if(xhr.status == 200) {
-          // console.log(xhr.responseText);
-          console.log(xhr.responseText);
-          retour = JSON.parse(xhr.responseText);
-        } else {
-          console.log("error : " + xhr.status);
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState == 4) {
+            if (xhr.status == 200) {
+                // console.log(xhr.responseText);
+                console.log(xhr.responseText);
+                retour = JSON.parse(xhr.responseText);
+            } else {
+                console.log("error : " + xhr.status);
+            }
         }
-      }
     };
 
-  xhr.open("POST", herf, false);
-  xhr.send(Data);
+    xhr.open("POST", herf, false);
+    xhr.send(Data);
 
-  return retour;
+    return retour;
 }
 
