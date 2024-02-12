1a64040b99c3ddda86d900d1b91bffd25444c5e2
tsoa <henintsoapaul@gmail.com>
2024-02-12 14:21:20 +0300

    Add Functs : get all rows : cueilleurs and varietes.

--- inc/php/crudFuncts/select.php
+++ inc/php/crudFuncts/select.php
@@ -1,5 +1,6 @@
 <?php
 
+// --- CHECK LOGIN ---
 /**
  * Return the number of row matching Pseudo+Password. If 1, SUCCESS. Else, FAILED.
  * @param string $pseudo
@@ -36,15 +37,76 @@ function checkLogin( $connection, $pseudo, $password, $idStatus )
 /**
  * Return 1 on SUCCESS.
  */
-function checkLoginAdmin($connection, $pseudo, $password)
+function checkLoginAdmin( $connection, $pseudo, $password )
 {
-    return checkLogin($connection, $pseudo, $password, 1);
+    return checkLogin( $connection, $pseudo, $password, 1 );
 }
 
 /**
  * Return 1 on SUCCESS.
  */
-function checkLoginUser($connection, $pseudo, $password)
+function checkLoginUser( $connection, $pseudo, $password )
 {
-    return checkLogin($connection, $pseudo, $password, 2);
+    return checkLogin( $connection, $pseudo, $password, 2 );
+}
+
+// --- SELECT ---
+/**
+ * @param $query string A valid select query.
+ * @return array|null An ARRAY OF ROWS from the query.
+ */
+function exeSelect( $connection, $query )
+{
+    try {
+        $result = mysqli_query( $connection, $query );
+
+        // Initialize an empty array to hold the rows
+        $utilisateurs = [];
+
+        // Loop through the result set and store each row as an associative array
+        while ( $row = mysqli_fetch_assoc( $result ) ) {
+            foreach ( $row as &$value ) {
+                // If the value looks like an integer, cast it to an integer.
+                if ( ctype_digit( (string) $value ) ) {
+                    $value = (int) $value;
+                }
+            }
+            $utilisateurs[] = $row;
+        }
+
+        // Return all utilisateur records
+        return $utilisateurs;
+    }
+    catch ( Exception $e ) {
+        // Handle the exception, e.g., log the error message
+        error_log( $e->getMessage() );
+        // Optionally, you can display the error message to the user in a user-friendly way
+        echo "An error occurred: " . htmlspecialchars( $e->getMessage() );
+    }
+    return null;
+}
+
+// SELECT ALL COLUMNS + ALL ROWS
+/**
+ * Return all columns of all rows in the specified table. Columns' name are the indexes of a row.
+ */
+function getAllRows( $connection, $tableName )
+{
+    if ( isset( $connection ) && isset( $tableName ) ) {
+        $query = "SELECT * FROM $tableName";
+        return exeSelect( $connection, $query );
+    } else {
+        echo "connection OR tableName cannot be null!";
+        return null;
+    }
+}
+
+function getAllCueilleurs( $connection )
+{
+    return getAllRows( $connection, "the_cueilleurs" );
+}
+
+function getAllVarietes( $connection )
+{
+    return getAllRows( $connection, "the_varietesthes" );
 }
