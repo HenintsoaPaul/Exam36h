4d5b4a14ecb9e4f2ae386315aaff329543178d65
tsoa <henintsoapaul@gmail.com>
2024-02-12 16:23:03 +0300

    Traitement addParcelle.php.

---
+++ pages/admin/traitements/addParcelle.php
@@ -0,0 +1,27 @@
+<?php
+require_once 'inc/php/connection.php';
+require_once 'inc/php/crudFuncts/create.php';
+
+// Verify inputs
+if ( !isset( $_POST['surface'] ) ) $error = "surface cannot be null!";
+elseif ( !isset( $_POST['idVariete'] ) ) $error = "idVariete cannot be null!";
+
+// Redirect if a value is null
+$link = "insertion-parcelle.php?message";
+if ( isset( $error ) ) {
+    header( "Location:$link=$error" );
+}
+
+$surface = $_POST['surface'];
+$idVariete = $_POST['idVariete'];
+
+// exe query
+$connection = db_connect();
+$nbRowsAdded = addParcelle( $connection, $surface, $idVariete );
+closeConnection( $connection );
+
+// redirection
+$message = $nbRowsAdded === 1 ? "success" : "failed";
+header("Location:$link=$message");
+
+?>
