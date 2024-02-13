1a69fe6dc13c33b78aa8780690905802e551583b
jca <118293506+JCA-STEPH@users.noreply.github.com>
2024-02-13 10:13:34 +0300

    paiement

--- pages/utilisateur/paiements.php
+++ pages/utilisateur/paiements.php
@@ -28,56 +28,97 @@
     <div class="container d-block" id="nullLayout">
         <div class="row">
             <div class="col-12 text-center">
-            <p> Aucun resultat</p>
+                <p> Aucun paiement </p>
             </div>
-            
         </div>
     </div>
-    <div class="container d-none" id="resultLayout"> 
+    <div class="container d-none" id="paiementLayout"> 
         <div class="row">
             <div class="col-12 col-lg-6">
                 <div class="card h-100">
                     <div class="card-body">
                         <h3>Poids restants au <span id="dateFinTitle">  </span> </h3>
-                        <table class="table" id="parcelleTable">
+                        <table class="table" id="paiementTable">
                             <thead>
                                 <tr>
-                                    <th>Parcelle</th>
-                                    <th>Poids restant</th>
+                                    <th>Date cueillette</th>
+                                    <th>Nom cueilleur</th>
+                                    <th>Poids</th>
+                                    <th>%Bonus</th>
+                                    <th>%Maallus</th>
+                                    <th>Montant paiement</th>
                                 </tr>
                             </thead>
-                            <tbody id="parcelleTableBody">
+                            <tbody id="paiementTableBody">
                                 
                             </tbody>
                         </table>
                     </div>
                 </div>
             </div>
-            <div class="col-12 col-lg-6  d-flex flex-column">
-                    <div class="card mb-md-3">
-                        <div class="card-header">
-                        </div>
-                        <div class="card-body text-center">
-                            <h3>Poids Total</h3>
-                            <p class="fw-bold fs-3"> <span id="poidsTotal"></span> <span class="text-success">Kg</span></p>
-                        </div>
-                        <div class="card-footer"></div>
-                    </div>
-                    <div class="card">
-                        <div class="card-header">
-                        </div>
-                        <div class="card-body text-center">
-                            <h3>Couts de revient</h3>
-                            <p class="fw-bold fs-3">  <span id="coutDeRevient">  </span> <span class="text-success">$</span></p>
-                        </div>
-                        <div class="card-footer"></div>
-                    </div>
-            </div>
         </div>
     </div>
 </section>
 <script>
+    var paiementForm = document.getElementById("paiementFormulaire");
+    var paiementLayout = document.getElementById("paiementLayout");
+    var nullLayout = document.getElementById("nullLayout");
+/// Les champs dates de filtre
+    var dateDebutInput = document.getElementById("dateDebutInput");
+    var dateFinInput = document.getElementById("dateFinInput");
+/// Action lors de la validation du formulaire
+    paiementForm.addEventListener("submit" , function(event){
+        event.preventDefault();
+    /// Traitement de donnee
+        var data ;
+        var tbody = document.getElementById("paiementTableBody");
+        tbody.innerHTML = "";
+        for (let index = 0; index < data.length; index++) {
 
+            const chunk = data[index];
+
+            var tr = document.createElement("tr");
+        /// Recuperration des valeurs necessaire
+            const dateCueillette = "";
+            const nomCueilleur = "";
+            const poids = "";
+            const bonus = "";
+            const mallus = "";
+            const montantPaiement = "";
+
+        /// Creation des  membres du tableau
+            var td_dateCueillette = document.createElement("td");
+            td_dateCueillette.textContent = dateCueillette;
+            tr.appendChild(td_dateCueillette);
+
+            var td_nomCueilleur = document.createElement("td");
+            td_nomCueilleur.textContent = nomCueilleur;
+            tr.appendChild(td_nomCueilleur);
+
+            var td_poids = document.createElement("td");
+            td_poids.textContent = poids;
+            tr.appendChild(td_poids);
+
+            var td_bonus = document.createElement("td");
+            td_bonus.textContent = bonus;
+            tr.appendChild(td_bonus);
+
+            var td_mallus = document.createElement("td");
+            td_mallus.textContent = mallus;
+            tr.appendChild(td_mallus);
+
+            var td_montantPaiement = document.createElement("td");
+            td_montantPaiement.textContent = montantPaiement;
+            tr.appendChild(td_montantPaiement);
+
+            tbody.appendChild(tr);
+        }
+    ///
+
+    /// Affichage de resultat
+        nullLayout.classList.replace("d-block" , "d-none");
+        paiementLayout.classList.replace("d-none" , "d-block");
+    });
 </script>
 <script>
     activeCurrentPage("paiements_li");
