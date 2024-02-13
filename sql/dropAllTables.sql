38745f9232d8afe31c6b8c6054f12058f0afb9cf
tsoa <henintsoapaul@gmail.com>
2024-02-13 09:19:25 +0300

    Script drop all tables.

---
+++ sql/dropAllTables.sql
@@ -0,0 +1,31 @@
+drop table the_bonus;
+
+drop table the_categoriesdepenses;
+
+drop table the_cueillettes;
+
+drop table the_cueilleurs;
+
+drop table the_depenses;
+
+drop table the_genres;
+
+drop table the_mallus;
+
+drop table the_mois;
+
+drop table the_parcelles;
+
+drop table the_poidsminimal;
+
+drop table the_prixvente;
+
+drop table the_regenerations;
+
+drop table the_salaires;
+
+drop table the_status;
+
+drop table the_users;
+
+drop table the_varietesthes;
