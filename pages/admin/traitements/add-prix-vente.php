25898601d1cd4773d6ae50056413e7bea87f6f08
sergianafr <sergiravelo@gmail.com>
2024-02-13 11:38:41 +0300

    IntegrationprixVente

--- pages/admin/traitements/add-prix-vente.php
+++ pages/admin/traitements/add-prix-vente.php
@@ -2,10 +2,12 @@
 require_once '../../../inc/php/connection.php';
 require_once '../../../inc/php/crudFuncts/create.php';
 
-if ( !isset( $_POST['surface'] ) ) $error = "surface cannot be null!";
+if ( !isset( $_POST['montantInput'] ) ) $error = "montant cannot be null!";
 elseif ( !isset( $_POST['idVariete'] ) ) $error = "idVariete cannot be null!";
+elseif ( !isset( $_POST['dateInput'] ) ) $error = "date cannot be null!";
 
-$link = "../insertion-parcelle.php?message";
+
+$link = "../insertion-prix-vente.php?message";
 if ( isset( $error ) ) {
     header( "Location:$link=$error" );
 }
@@ -15,7 +17,7 @@ $idVariete = $_POST['idVariete'];
 $date = $_POST['dateInput'];
 // exe query
 $connection = db_connect();
-$nbRowsAdded = addPrixVente( $connection, $montant, $idVariete, $date );
+$nbRowsAdded = addPrixRevient( $connection, $montant, $date, $idVariete );
 closeConnection( $connection );
 
 // redirection
