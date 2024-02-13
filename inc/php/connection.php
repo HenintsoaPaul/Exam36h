a53769ae1cffe6dfc804fbe8a7ee467a8efa548d
jca <118293506+JCA-STEPH@users.noreply.github.com>
2024-02-13 16:39:35 +0300

    connection to database

--- inc/php/connection.php
+++ inc/php/connection.php
@@ -2,7 +2,7 @@
     function db_connect() {
         static $connect = null;
         if( $connect === null ) {
-            $connect = mysqli_connect( 'localhost','root','root','the' );
+            $connect = mysqli_connect( '172.10.0.113','ETU002434','6H3zGFFxLndm','db_p16_ETU002434' );
         }
         return $connect;
     }
