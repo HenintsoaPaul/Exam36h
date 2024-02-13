bad338e61bcd9342429c0d8f9455267979a809c5
jca <118293506+JCA-STEPH@users.noreply.github.com>
2024-02-13 16:49:20 +0300

    lowercase

--- sql/data.sql
+++ sql/data.sql
@@ -1,61 +1,60 @@
-INSERT INTO the_Status (NomStatu)
+INSERT INTO the_status (NomStatu)
 VALUES ('Admin'),
        ('User');
 
-INSERT INTO the_Genres (NomGenre)
+INSERT INTO the_genres (NomGenre)
 VALUES ('Homme'),
        ('Femme'),
        ('Non Binaire');
 
-INSERT INTO the_VarietesThes (NomVariete, Occupation, RendementParPied)
+INSERT INTO the_varietesthes (NomVariete, Occupation, RendementParPied)
 VALUES ('Vert', 500, 4),
        ('Noir', 1000, 1),
        ('Bad Guyz', 250, 2);
 
-INSERT INTO the_CategoriesDepenses (NomCategorie)
+INSERT INTO the_categoriesdepenses (NomCategorie)
 VALUES ('Engrais'),
        ('Carburant'),
        ('Logistique');
 
-INSERT INTO the_Parcelles (Surface, idVarieteThe)
+INSERT INTO the_parcelles (Surface, idVarieteThe)
 VALUES (3, 1),
        (2, 2),
        (2, 3);
 
-INSERT INTO the_Cueilleurs (Nom, DateNaissance, idGenre)
+INSERT INTO the_cueilleurs (Nom, DateNaissance, idGenre)
 VALUES ('Alex', '2000-02-09', 1),
        ('Henintsoa', '2000-02-03', 1);
 
-INSERT INTO the_Users (pseudo, password, idstatu)
+INSERT INTO the_users (pseudo, password, idstatu)
 VALUES ('alexAdmin', 'admin', 1),
        ('henints', 'user', 2);
-INSERT INTO the_Users (pseudo, password, idstatu)
-VALUES ('Sergiana', 'user', 2);
+       ('Sergiana', 'user', 2);
 
-INSERT INTO the_Mois (NomMois)
+INSERT INTO the_mois (NomMois)
 VALUES ('Janvier'), ('Fevrier'), ('Mars'), ('Avril'), ('Mai'), ('Juin'),
        ('Juillet'), ('Aout'), ('Septembre'), ('Octobre'), ('Novembre'), ('Decembre');
 
-INSERT INTO the_Regenerations (idMois, idVarieteThe)
+INSERT INTO the_regenerations (idMois, idVarieteThe)
 VALUES (1, 1),
        (6, 1);
 
-INSERT INTO the_Mallus (Mallus, DateConfig)
+INSERT INTO the_mallus (Mallus, DateConfig)
 VALUES (10, '2024-01-01');
 
-INSERT INTO the_Bonus (Bonus, DateConfig)
+INSERT INTO the_bonus (Bonus, DateConfig)
 VALUES (10, '2024-01-01');
 
-INSERT INTO the_PoidsMinimal (Poids, DateConfig)
+INSERT INTO the_poidsminimal (Poids, DateConfig)
 VALUES (100, '2024-01-01');
 
-INSERT INTO the_Depenses (DateDepense, MontantDepense, idCategorieDepense)
+INSERT INTO the_depenses (DateDepense, MontantDepense, idCategorieDepense)
 VALUES ('2024-01-01', 10, 1);
 
-INSERT INTO the_PrixVente (MontantPrixVente, dateConfig, idVarieteThe)
+INSERT INTO the_prixvente (MontantPrixVente, dateConfig, idVarieteThe)
 VALUES (100, '2024-01-01', 1),
        (100, '2024-01-01', 2),
        (100, '2024-01-01', 3);
 
-INSERT INTO the_Salaires (salaire, DateDebutSalaire)
+INSERT INTO the_salaires (salaire, DateDebutSalaire)
 VALUES (50, '2024-01-01');
--- sql/dropAllTables.sql
+++ sql/dropAllTables.sql
@@ -1,31 +1,31 @@
 drop table the_bonus;
 
-drop table the_CategoriesDepenses;
+drop table the_categoriesdepenses;
 
-drop table the_Cueillettes;
+drop table the_cueillettes;
 
-drop table the_Cueilleurs;
+drop table the_cueilleurs;
 
-drop table the_Depenses;
+drop table the_depenses;
 
-drop table the_Genres;
+drop table the_genres;
 
-drop table the_Mallus;
+drop table the_mallus;
 
-drop table the_Mois;
+drop table the_mois;
 
-drop table the_Parcelles;
+drop table the_parcelles;
 
-drop table the_PoidsMinimal;
+drop table the_poidsminimal;
 
-drop table the_PrixVente;
+drop table the_prixvente;
 
-drop table the_Regenerations;
+drop table the_regenerations;
 
-drop table the_Salaires;
+drop table the_salaires;
 
-drop table the_Status;
+drop table the_status;
 
-drop table the_Users;
+drop table the_users;
 
-drop table the_VarietesThes;
+drop table the_varietesthes;
