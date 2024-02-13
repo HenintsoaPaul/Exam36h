c46a7a01e2843e5e624a768208fb0d4e6c6422ed
jca <118293506+JCA-STEPH@users.noreply.github.com>
2024-02-13 09:04:13 +0300

    prix de vente formulaire

--- pages/admin/static/header.php
+++ pages/admin/static/header.php
@@ -24,47 +24,53 @@
                   <div class="d-flex"></div>
                   <ul class="navbar-nav col-2 flex-grow-1 gap-2 justify-content-center mr-auto mt-2 mt-lg-0">
                     <li class="nav-item">
-                          <a class="nav-link" id="admin_li" href="home.php">Admin home<span class="sr-only"></span></a>
-                        </li>
-                        <li class="nav-item">
-                          <a class="nav-link" id="variete_li" href="insertion-variete.php">Variete</a>
-                        </li>
-                        <li class="nav-item">
-                          <a class="nav-link" id="parcelle_li" href="insertion-parcelle.php">Parcelle</a>
-                        </li>
-                        <li class="nav-item">
+                      <a class="nav-link" id="admin_li" href="home.php">Admin home<span class="sr-only"></span></a>
+                    </li>
+                    
+                    <li class="nav-item">
+                      <a class="nav-link" id="variete_li" href="insertion-variete.php">Variete</a>
+                    </li>
+                    
+                    <li class="nav-item">
+                      <a class="nav-link" id="parcelle_li" href="insertion-parcelle.php">Parcelle</a>
+                    </li>
 
-                          <a class="nav-link" id="ceuilleur_li" href="insertion-cueilleur.php">Ceuilleur</a>
-                        </li>
-                        <li class="nav-item dropdown">
-                          <a class="nav-link dropdown-toggle" href="#" data-target="configs" id="configDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                            Configuration
-                          </a>
-                          <div class="dropdown-menu bg-light" aria-labelledby="configDropdown" id="configs" style="width:200px;">
-                            <ul class="nav gap-3">
-                              <li class="nav-item">
-                                <a class="nav-link text-dark" id="configPoids_li" href="insertion-config-poids-min.php">Config poids minimal</a>
-                              </li>
-                              
-                              <li class="nav-item">
-                                <a class="nav-link text-dark" id="configMalus_li" href="insertion-config-malus.php">Config malus</a>
-                              </li>
-                              <li class="nav-item">
-                                <a class="nav-link text-dark" id="configBonus_li" href="insertion-config-bonus.php">Config bonus</a>
-                              </li>
-                            </ul>      
-                          </div>
-                        </li>
-                        
-              
-                        <li class="nav-item">
-                        </li>
-                        <li class="nav-item">
-                          <a class="nav-link" id="categorie_li" href="insertion-categorie.php">Categorie</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" id="salaire_li" href="insertion-salaire.php">Salaire</a>
+                    <li class="nav-item">
+                      <a class="nav-link" id="ceuilleur_li" href="insertion-cueilleur.php">Ceuilleur</a>
+                    </li>
+                    <li class="nav-item dropdown">
+                      <a class="nav-link dropdown-toggle" href="#" data-target="configs" id="configDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        Configuration
+                      </a>
+                      <div class="dropdown-menu bg-light" aria-labelledby="configDropdown" id="configs" style="width:200px;">
+                        <ul class="nav gap-3">
+                          <li class="nav-item">
+                            <a class="nav-link text-dark" id="configPoids_li" href="insertion-config-poids-min.php">Config poids minimal</a>
                           </li>
+                          
+                          <li class="nav-item">
+                            <a class="nav-link text-dark" id="configMalus_li" href="insertion-config-malus.php">Config malus</a>
+                          </li>
+
+                          <li class="nav-item">
+                            <a class="nav-link text-dark" id="configBonus_li" href="insertion-config-bonus.php">Config bonus</a>
+                          </li>
+                        </ul>      
+                      </div>
+                    </li>
+                    
+          
+                    <li class="nav-item">
+                      <a class="nav-link" id="prixVente_li" href="insertion-prix-vente.php">Prix de vente</a>
+                    </li>
+
+                    <li class="nav-item">
+                      <a class="nav-link" id="categorie_li" href="insertion-categorie.php">Categorie</a>
+                    </li>
+
+                    <li class="nav-item">
+                        <a class="nav-link" id="salaire_li" href="insertion-salaire.php">Salaire</a>
+                      </li>
                     </ul>
                     <div class="rounded d-inline bg-light">
                       <a href="logout.php" type="button" class="btn hover-fill transition-fill">Deconnexion</a>
